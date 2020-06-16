58eff60ac0ef25930536628ddd13ac2fcb82ad4f
sadique-cws <sadique.hussain96@gmail.com>
2020-06-16 11:03:02 +0530

    second commit

--- public/urls.py
+++ public/urls.py
@@ -4,4 +4,6 @@ from django.urls import path,include
 from public.views import *
 urlpatterns = [
     path('',HomeView.as_view(),name='homepage'),
+    path('add/',AddRecordView.as_view(),name='addRecord'),
+    path('details/<int:pk>/',PeopleDetailView.as_view(),name='item'),
 ]
--- public/views.py
+++ public/views.py
@@ -1,18 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .models import *
-
-from django.views.generic import ListView,View
-
-
-
-# def Home(requst):
-#     data = {"home":Peoples.objects.all()}
-#     return render(requst,"home.html",data)
-
-
+from .forms import *
+from django.views.generic import ListView,View,DetailView
 
 class HomeView(View):
-
     def get(self,request,*args,**kwargs):
         data = {
             "peoples":Peoples.objects.all(),
@@ -21,4 +12,27 @@ class HomeView(View):
         return render(self.request,"home.html",data)
 
 
+class PeopleDetailView(DetailView):
+    model = Peoples
+    template_name = "item.html"
 
+    def get_context_data(self, **kwargs):
+        context = super(PeopleDetailView, self).get_context_data(**kwargs)
+        context['categories'] = Categories.objects.all()
+        return context
+
+class AddRecordView(View):
+    form = AddRecordForm()
+
+    def get(self,request,*args,**kwargs):
+        data= {"form":self.form}
+        return render(self.request,"addRecord.html",data)
+
+    def post(self,request,*args,**kwargs):
+        form = AddRecordForm(self.request.POST or None)
+        if form.is_valid():
+            form.save()
+            return redirect("homepage")
+        else:
+            #todo : if not form is valid then what
+            pass
